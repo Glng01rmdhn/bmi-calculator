8a5d307fcfdfd26feaa80173ee467c2b42b48a8b
Hektor-alfatih <hektor2310@student.abudzar.sch.id>
2025-05-19 11:06:15 +0700

    ups

--- bmi-calculator/src/Detail.jsx
+++ bmi-calculator/src/Detail.jsx
@@ -1,12 +1,11 @@
 // src/Detail.jsx
 function Detail() {
   return (
-    <div style={{ padding: "20px" }}>
-      <h1>Detail BMI</h1>
+    <div style={{ padding: "20px", color: "green" }}>
+      <h1>Detail BMI (penjelasan hektor a;fatih)</h1>
       <p>BMI (Body Mass Index) adalah ukuran yang digunakan untuk menilai apakah berat badan seseorang sesuai dengan tinggi badannya.</p>
     </div>
   );
 }
 
 export default Detail;
-
